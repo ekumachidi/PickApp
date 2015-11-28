ec14a40ed5deca6dc2696880c1218bcc2c28f587
ekumachidi <bukki.arise@yahoo.co.uk>
2015-11-28 15:04:04 +0000

    Add schema.rb after first migration

---
+++ db/schema.rb
@@ -0,0 +1,96 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20151128142231) do
+
+  create_table "assignments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "package_id"
+    t.integer  "courier_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "assignments", ["courier_id"], name: "index_assignments_on_courier_id"
+  add_index "assignments", ["package_id"], name: "index_assignments_on_package_id"
+  add_index "assignments", ["user_id"], name: "index_assignments_on_user_id"
+
+  create_table "couriers", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "couriers", ["user_id"], name: "index_couriers_on_user_id"
+
+  create_table "packages", force: :cascade do |t|
+    t.string   "tracking_code",                 null: false
+    t.integer  "weight",                        null: false
+    t.string   "vendor",                        null: false
+    t.string   "location",                      null: false
+    t.string   "destination"
+    t.string   "recipient"
+    t.string   "r_contact"
+    t.time     "due_at"
+    t.boolean  "status",        default: false
+    t.boolean  "dispatch",      default: false
+    t.boolean  "assigned",      default: false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.integer  "user_id"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
+  add_index "packages", ["user_id"], name: "index_packages_on_user_id"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "address",    null: false
+    t.string   "phone",      null: false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.integer  "role_id"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["role_id"], name: "index_users_on_role_id"
+
+end
