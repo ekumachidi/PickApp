e0a9fe523cd817be9c16601133eea2823f4e7b5b
ekumachidi <bukki.arise@yahoo.co.uk>
2015-11-29 02:05:14 +0000

    Add index template for landing page

---
+++ app/controllers/packages_controller.rb
@@ -0,0 +1,65 @@
+class PackagesController < ApplicationController
+  before_action :set_package, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @packages = Package.all
+    @hash = Gmaps4rails.build_markers(@packages) do |package, marker|
+    marker.lat package.latitude
+    marker.lng package.longitude
+    marker.infowindow package.tracking_code
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @package = Package.new
+  end
+
+  def edit
+  end
+
+  def create
+    @package = Package.new(package_params)
+
+    respond_to do |format|
+      if @package.save
+        format.html { redirect_to @package, notice: 'Package was successfully created.' }
+        format.json { render :show, status: :created, location: @package }
+      else
+        format.html { render :new }
+        format.json { render json: @package.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @package.update(package_params)
+        format.html { redirect_to @package, notice: 'Package was successfully updated.' }
+        format.json { render :show, status: :ok, location: @package }
+      else
+        format.html { render :edit }
+        format.json { render json: @package.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @package.destroy
+    respond_to do |format|
+      format.html { redirect_to packages_url, notice: 'Package was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_package
+      @package = Package.find(params[:id])
+    end
+
+    def package_params
+      params[:package]
+    end
+end
