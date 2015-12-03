3fa423b4e81f71732b43398e9a4f67849ef36bf6
ekumachidi <bukki.arise@yahoo.co.uk>
2015-12-03 13:42:30 +0000

    Add Profile controller and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(users)
+  	# current_user_path
+  	# @user_id = current_user.id
+  	if current_user.sign_in_count == 0
+  		user_profiles_path(current_user.id)
+  	else
+  		user_packages_path(current_user.id)
+  	end
+  end
+
 end
